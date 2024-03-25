88af3fec9538af37d69b70fb2fc0c6c3473642a8
xXHemxaXx <mimi.hemxa@gmail.com>
2024-03-25 09:19:38 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// alert("Hello World")
+// console.log("Hello world");
+
+const monPrenom = "Hemza"
+let monAge = 26
+
+// console.log("Mon prenom est " + monPrenom + " et j'ai " + monAge + " ans.");
+console.log(`Mon prenom est ${monPrenom} et j'ai ${monAge} ans.`);
+
+monAge = 35
+
+console.log(`Mon prenom est ${monPrenom} et j'ai ${monAge} ans.`);
