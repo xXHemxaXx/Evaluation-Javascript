d16b986fc7f07ca466960b0b60a101500ee01cd2
xXHemxaXx <mimi.hemxa@gmail.com>
2024-03-25 15:56:47 +0100

    HTML et CSS TERMINER ('En cours sur le javascript 3-a') commit

--- js/index.js
+++ js/index.js
@@ -1,32 +1,72 @@
 // Variable //
-
 let containerListeClub = document.querySelector("#container-liste-club-foot")
-let btnsMise = document.querySelectorAll("button")
 
 // Affiche les matchs //
 fetch('js/datas.json')
     .then(response => response.json())
     .then(data => {
     console.log(data);
-        // Boucle pour affiche les matchs
-        for (let i = 0; i < data.matchs.length; i++) {
+        // Variable dans le fetch //
+        
+        // Boucle pour affiche les matchs//
+        data.matchs.forEach(match => {
             containerListeClub.innerHTML += `
             <div class="container-club-de-foot">
                 <div class="container-club-de-foot-nom">
-                    <p>${data.matchs[i].hometeam} - ${data.matchs[i].awayteam}</p>
+                    <p>${match.hometeam} - ${match.awayteam}</p>
                 </div>
                 <div class="container-button">
-                    <button>${data.matchs[i].home_odd}</button>
-                    <button>${data.matchs[i].draw_odd}</button>
-                    <button>${data.matchs[i].away_odd}</button>
+                    <button class="btn">${match.home_odd}</button>
+                    <button class="btn">${match.draw_odd}</button>
+                    <button class="btn">${match.away_odd}</button>
                 </div>
             </div>
             `
-        }
+        });
+        // Events //
+        // containerListeClub.addEventListener('click', function(e){
+        //     let containerButton = document.querySelector(".container-button")
+        //     // On check si on est bien sur un bouton
+        //     if(e.target.classList.contains('btn')) {
+        //         // On vérifie si l'élément clické a déjà une classe focusButton ... 
+        //         if (e.target.classList.contains('focusButton')){
+        //             e.target.classList.remove('focusButton')
+        //         } 
+        //         // On vérifie si l'élément clické n'a pas déjà une classe focusButton ... 
+        //         else {
+        //             // Si mon containerListeClub a un autre enfant que celui sur lequel je suis en train de cliquer qui possède déjà la classe focusButton
+        //             if(containerListeClub.querySelector('.focusButton')) {
+        //                 containerListeClub.querySelector('.focusButton').classList.remove('focusButton')
+        //                 e.target.classList.add('focusButton')
+        //             // Si mon containerListeClub n'a aucun enfant qui a la classe focusButton alors ...
+        //             } else {
+        //                 e.target.classList.add('focusButton')
+        //             }
+        //         }
+        //     }
+        // })
+        let btns = document.querySelectorAll(".btn")
+        containerListeClub.addEventListener('click', function(){
+            btns.forEach(btn => {
+                
+            });
+        })
     })
     .catch(error => {console.log("Erreur lors de la récup des données :", error);
 })
 
-// btnsMise.addEventListener('toggle', function {
-//     btnsMise.classList.toggle("focusButton")
-// })
+// // Variable DarkMode //
+// let tableauxDark = [
+//     dark = {
+//         svg : `${}`
+//     },
+//     light = {
+//         svg : `${}`
+//     }
+// ]
+
+// localStorage.setItem('darkmode', JSON.stringify(tableauxDark))
+
+
+// // Event DarkMode //
+// let darkMode = document.querySelector(".darkmode")
